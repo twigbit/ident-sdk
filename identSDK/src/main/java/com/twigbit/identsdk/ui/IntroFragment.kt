29df5178db6f476a122ce73f56e893d1fe4ae262
Moritz Morgenroth <development@moritzmorgenroth.de>
2019-01-22 11:04:42 +0100

    Scaffold drop in ui fragments

---
+++ identSDK/src/main/java/com/twigbit/identsdk/ui/IntroFragment.kt
@@ -0,0 +1,26 @@
+package com.twigbit.identsdk.ui
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.twigbit.identsdk.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class IntroFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro, container, false)
+    }
+
+}
