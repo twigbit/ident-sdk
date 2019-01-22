74e43a03b375268eeb8410c8a1ce1fc131283c58
Moritz Morgenroth <development@moritzmorgenroth.de>
2019-01-22 11:22:51 +0100

    Create intent builder

---
+++ identSDK/src/main/java/com/twigbit/identsdk/DropinRequest.kt
@@ -0,0 +1,15 @@
+package com.twigbit.identsdk
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import com.twigbit.identsdk.ui.DropinIdentificationActivity
+
+class DropinRequest(private val clientToken: String){
+    val EXTRA_CLIENT_TOKEN = "twigbit-ident-extra-client-token"
+    fun getIntent(context: Context): Intent{
+        val intent = Intent(context, DropinIdentificationActivity::class.java)
+        intent.putExtra(EXTRA_CLIENT_TOKEN, clientToken)
+        return intent
+    }
+}
--- sample/src/main/java/com/twigbit/identsdk/sample/MainActivity.kt
+++ sample/src/main/java/com/twigbit/identsdk/sample/MainActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.twigbit.identsdk.IdentificationActivity
 import com.twigbit.identsdk.Message
+import android.content.Intent.getIntent
+
+
 
 class MainActivity : IdentificationActivity() {
     override fun onError(message: String) {
@@ -21,5 +24,12 @@ class MainActivity : IdentificationActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    val REQUEST_CODE_IDENTIFICATION = 0;
+    private fun startDropInIdentification(){
+        val dropInRequest = DropInRequest()
+            .clientToken("RmluZ2VycHJpbnQiOiI")
+        startActivityForResult(dropInRequest.getIntent(this), REQUEST_CODE_IDENTIFICATION)
     }
 }
