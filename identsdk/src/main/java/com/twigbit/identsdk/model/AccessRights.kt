fdd0492444277ee183caaee9a3854c2b221b0938
Moritz Morgenroth <development@moritzmorgenroth.de>
2019-05-14 10:03:08 +0200

    Refactor access rights into seperate model file, fix translation

---
+++ identsdk/src/main/java/com/twigbit/identsdk/model/AccessRights.kt
@@ -0,0 +1,18 @@
+package com.twigbit.identsdk.model
+
+object AccessRights{
+    const val BIRTH_NAME = "BirthName"
+    const val FAMILY_NAME = "FamilyName"
+    const val GIVEN_NAMES = "GivenNames"
+    const val DATE_OF_BIRTH = "DateOfBirth"
+    const val PLACE_OF_RECIDENCE = "PlaceOfResidence"
+    const val NATIONALITY = "Nationality"
+    const val ACADEMIC_TITLE = "AcademicTitle"
+    const val ARTISTIC_NAME = "ArtisticName"
+    const val ISSUING_STATE = "IssuingState"
+    const val RESTRICTED_ID = "RestrictedID"
+    const val PLACE_OF_BIRTH = "PlaceOfBirth"
+    const val DOCUMENT_TYPE = "DocumentType"
+    const val RESIDENT_PERMIT = "ResidencePermitI"
+    const val DATE_OF_EXPIRY = "DateOfExpiry"
+}
--- identsdk/src/main/java/com/twigbit/identsdk/util/StringUtil.kt
+++ identsdk/src/main/java/com/twigbit/identsdk/util/StringUtil.kt
@@ -3,23 +3,24 @@ package com.twigbit.identsdk.util
 import android.content.Context
 import com.twigbit.identsdk.R
 import com.twigbit.identsdk.ausweisident.AusweisIdentScopes
+import com.twigbit.identsdk.model.AccessRights
 
 object StringUtil{
     fun translate(context: Context, string: String): String{
         val id = when (string){
-            AusweisIdentScopes.ACADEMIC_TITLE -> R.string.access_right_academic_title
-            AusweisIdentScopes.ARTISTIC_NAME -> R.string.access_right_artistic_name
-            AusweisIdentScopes.BIRTH_NAME -> R.string.access_right_birth_name
-            AusweisIdentScopes.DATE_OF_BIRTH -> R.string.access_right_date_of_birth
-            AusweisIdentScopes.DATE_OF_EXPIRY -> R.string.access_right_date_of_expiry
-            AusweisIdentScopes.DOCUMENT_TYPE -> R.string.access_right_doc_type
-            AusweisIdentScopes.FAMILY_NAME -> R.string.access_right_family_name
-            AusweisIdentScopes.GIVEN_NAMES -> R.string.access_right_given_name
-            AusweisIdentScopes.ISSUING_STATE -> R.string.access_right_issuing_state
-            AusweisIdentScopes.NATIONALITY -> R.string.access_right_nationality
-            AusweisIdentScopes.RESTRICTED_ID -> R.string.access_right_restricted_id
-            AusweisIdentScopes.PLACE_OF_BIRTH -> R.string.access_right_place_of_pirth
-            AusweisIdentScopes.PLACE_OF_RECIDENCE -> R.string.access_right_place_of_residence
+            AccessRights.ACADEMIC_TITLE -> R.string.access_right_academic_title
+            AccessRights.ARTISTIC_NAME -> R.string.access_right_artistic_name
+            AccessRights.BIRTH_NAME -> R.string.access_right_birth_name
+            AccessRights.DATE_OF_BIRTH -> R.string.access_right_date_of_birth
+            AccessRights.DATE_OF_EXPIRY -> R.string.access_right_date_of_expiry
+            AccessRights.DOCUMENT_TYPE -> R.string.access_right_doc_type
+            AccessRights.FAMILY_NAME -> R.string.access_right_family_name
+            AccessRights.GIVEN_NAMES -> R.string.access_right_given_name
+            AccessRights.ISSUING_STATE -> R.string.access_right_issuing_state
+            AccessRights.NATIONALITY -> R.string.access_right_nationality
+            AccessRights.RESTRICTED_ID -> R.string.access_right_restricted_id
+            AccessRights.PLACE_OF_BIRTH -> R.string.access_right_place_of_pirth
+            AccessRights.PLACE_OF_RECIDENCE -> R.string.access_right_place_of_residence
             else -> R.string.unknown_value
         }
         return context.getString(id)
