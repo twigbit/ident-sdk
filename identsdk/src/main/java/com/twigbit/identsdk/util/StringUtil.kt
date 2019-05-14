918f483d9ab2ebb8e4cdb1e1e4156e2242dcae03
Moritz Morgenroth <development@moritzmorgenroth.de>
2019-05-14 09:52:34 +0200

    Translate access rights

--- identsdk/src/main/java/com/twigbit/identsdk/util/StringUtil.kt
+++ identsdk/src/main/java/com/twigbit/identsdk/util/StringUtil.kt
@@ -7,19 +7,19 @@ import com.twigbit.identsdk.ausweisident.AusweisIdentScopes
 object StringUtil{
     fun translate(context: Context, string: String): String{
         val id = when (string){
-            AusweisIdentScopes.ACADEMIC_TITLE -> R.string.access_right_academit_title
-            AusweisIdentScopes.ARTISTIC_NAME -> R.string.access_right_academit_title
-            AusweisIdentScopes.BIRTH_NAME -> R.string.access_right_academit_title
-            AusweisIdentScopes.DATE_OF_BIRTH -> R.string.access_right_academit_title
-            AusweisIdentScopes.DATE_OF_EXPIRY -> R.string.access_right_academit_title
-            AusweisIdentScopes.DOCUMENT_TYPE -> R.string.access_right_academit_title
-            AusweisIdentScopes.FAMILY_NAME -> R.string.access_right_academit_title
-            AusweisIdentScopes.GIVEN_NAMES -> R.string.access_right_academit_title
-            AusweisIdentScopes.ISSUING_STATE -> R.string.access_right_academit_title
-            AusweisIdentScopes.NATIONALITY -> R.string.access_right_academit_title
-            AusweisIdentScopes.RESTRICTED_ID -> R.string.access_right_academit_title
-            AusweisIdentScopes.PLACE_OF_BIRTH -> R.string.access_right_academit_title
-            AusweisIdentScopes.PLACE_OF_RECIDENCE -> R.string.access_right_academit_title
+            AusweisIdentScopes.ACADEMIC_TITLE -> R.string.access_right_academic_title
+            AusweisIdentScopes.ARTISTIC_NAME -> R.string.access_right_artistic_name
+            AusweisIdentScopes.BIRTH_NAME -> R.string.access_right_birth_name
+            AusweisIdentScopes.DATE_OF_BIRTH -> R.string.access_right_date_of_birth
+            AusweisIdentScopes.DATE_OF_EXPIRY -> R.string.access_right_date_of_expiry
+            AusweisIdentScopes.DOCUMENT_TYPE -> R.string.access_right_doc_type
+            AusweisIdentScopes.FAMILY_NAME -> R.string.access_right_family_name
+            AusweisIdentScopes.GIVEN_NAMES -> R.string.access_right_given_name
+            AusweisIdentScopes.ISSUING_STATE -> R.string.access_right_issuing_state
+            AusweisIdentScopes.NATIONALITY -> R.string.access_right_nationality
+            AusweisIdentScopes.RESTRICTED_ID -> R.string.access_right_restricted_id
+            AusweisIdentScopes.PLACE_OF_BIRTH -> R.string.access_right_place_of_pirth
+            AusweisIdentScopes.PLACE_OF_RECIDENCE -> R.string.access_right_place_of_residence
             else -> R.string.unknown_value
         }
         return context.getString(id)
