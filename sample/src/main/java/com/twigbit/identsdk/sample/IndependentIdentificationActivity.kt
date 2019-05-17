efdd3520f77b844903bc3d8aba21cbefcddc5ae2
Moritz Morgenroth <development@moritzmorgenroth.de>
2019-05-17 10:10:31 +0200

    Create independent sample activity for testing

---
+++ sample/src/main/java/com/twigbit/identsdk/sample/IndependentIdentificationActivity.kt
@@ -0,0 +1,12 @@
+package com.twigbit.identsdk.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IndependentIdentificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_independent_identification)
+    }
+}
