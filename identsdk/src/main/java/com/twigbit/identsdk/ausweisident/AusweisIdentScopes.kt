2016257e1a3e2481081136fd652da5153c9a9d2f
Emil <emil.bergold@gmail.com>
2019-05-24 13:23:21 +0200

    corrected scopes and access rights

--- identsdk/src/main/java/com/twigbit/identsdk/ausweisident/AusweisIdentScopes.kt
+++ identsdk/src/main/java/com/twigbit/identsdk/ausweisident/AusweisIdentScopes.kt
@@ -1,12 +1,11 @@
 package com.twigbit.identsdk.ausweisident
 
 object AusweisIdentScopes {
-
     const val BIRTH_NAME = "BirthName"
-    const val FAMILY_NAME = "FamilyNames"
+    const val FAMILY_NAMES = "FamilyNames"
     const val GIVEN_NAMES = "GivenNames"
     const val DATE_OF_BIRTH = "DateOfBirth"
-    const val PLACE_OF_RECIDENCE = "PlaceOfResidence"
+    const val PLACE_OF_RESIDENCE = "PlaceOfResidence"
     const val NATIONALITY = "Nationality"
     const val ACADEMIC_TITLE = "AcademicTitle"
     const val ARTISTIC_NAME = "ArtisticName"
@@ -14,6 +13,6 @@ object AusweisIdentScopes {
     const val RESTRICTED_ID = "RestrictedID"
     const val PLACE_OF_BIRTH = "PlaceOfBirth"
     const val DOCUMENT_TYPE = "DocumentType"
-    const val RESIDENT_PERMIT = "ResidencePermitI"
+    const val RESIDENCE_PERMIT_I = "ResidencePermitI"
     const val DATE_OF_EXPIRY = "DateOfExpiry"
 }
--- identsdk/src/main/java/com/twigbit/identsdk/core/AccessRights.kt
+++ identsdk/src/main/java/com/twigbit/identsdk/core/AccessRights.kt
@@ -1,18 +1,23 @@
 package com.twigbit.identsdk.core
 
 object AccessRights{
+    const val ADDRESS = "Address"
     const val BIRTH_NAME = "BirthName"
     const val FAMILY_NAME = "FamilyName"
     const val GIVEN_NAMES = "GivenNames"
-    const val DATE_OF_BIRTH = "DateOfBirth"
-    const val PLACE_OF_RECIDENCE = "PlaceOfResidence"
-    const val NATIONALITY = "Nationality"
-    const val ACADEMIC_TITLE = "AcademicTitle"
-    const val ARTISTIC_NAME = "ArtisticName"
-    const val ISSUING_STATE = "IssuingState"
-    const val RESTRICTED_ID = "RestrictedID"
     const val PLACE_OF_BIRTH = "PlaceOfBirth"
+    const val DATE_OF_BIRTH = "DateOfBirth"
+    const val DOCTORAL_DEGREE = "DoctoralDegree"
+    const val ARTISTIC_NAME = "ArtisticName"
+    const val PSEUDONYM = "Pseudonym"
+    const val VALID_UNTIL = "ValidUntil"
+    const val NATIONALITY = "Nationality"
+    const val ISSUING_COUNTRY = "IssuingCountry"
     const val DOCUMENT_TYPE = "DocumentType"
-    const val RESIDENT_PERMIT = "ResidencePermitI"
-    const val DATE_OF_EXPIRY = "DateOfExpiry"
+    const val RESIDENCE_PERMIT_I = "ResidencePermitI"
+    const val RESIDENCE_PERMIT_II = "ResidencePermitII"
+    const val COMMUNITY_ID = "CommunityID"
+    const val ADDRESS_VERIFICATION = "AddressVerification"
+    const val AGE_VERIFICATION = "AgeVerification"
 }
+
